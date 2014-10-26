8927364ecb6cc8b2793061174a47784c6f9e92d6
yuliayu <test@TESTs-MacBook-Pro.local>
2014-10-27 01:37:23 +0200

    Homework updated
    added test fro wrong email verifying

--- src/test/java/simple/CheckEmail.java
+++ src/test/java/simple/CheckEmail.java
@@ -14,4 +14,11 @@ public class CheckEmail {
     {
         Assert.assertTrue(ValidateEmail.checkEmail(p), "Incorrect email format of " + p);
     }
+
+    @Parameters ({"wrong_email"})
+    @Test
+    public void failedEmail (String p)
+    {
+        Assert.assertFalse(ValidateEmail.checkEmail(p));
+    }
 }
