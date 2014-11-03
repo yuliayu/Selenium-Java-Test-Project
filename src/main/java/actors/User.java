5228c83ad07f5dcf857d2ed1cb1d2233c65e0bb5
yuliayu <test@TESTs-MacBook-Pro.local>
2014-11-03 03:28:25 +0200

    Homework 5
    version2

--- src/main/java/actors/User.java
+++ src/main/java/actors/User.java
@@ -4,9 +4,9 @@ package actors;
  * Created by test on 11/3/14.
  */
 public class User {
+    public String email;
     public String name;
     public String passwd;
     public String confirmPasswd;
-    public String email;
 
 }
